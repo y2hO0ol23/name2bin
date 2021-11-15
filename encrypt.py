d4fc22fa91521dcd5c61725a6a376a81e3e4d401
yyhyuk <74889162+yyhyuk@users.noreply.github.com>
2021-11-16 00:29:22 +0900

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,2 +1,13 @@
-name = "익명"
-print(bin(ord(name[0]))[2:].zfill(16) + bin(ord(name[1]))[2:].zfill(16))
+name = ""
+if len(name) <= 3:
+    name += "디코이름난독화"
+if len(name) % 2 == 1:
+    name += "디코이름난독화"
+
+result = [0 for _ in range(32)]
+for i in range(0,len(name),2):
+    bindata = list(bin(ord(name[i]))[2:].zfill(16) + bin(ord(name[i+1]))[2:].zfill(16))
+    for j in range(32):
+        result[j] ^= int(bindata[j])
+
+print("".join(map(str,result)))
